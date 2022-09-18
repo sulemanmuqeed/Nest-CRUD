ef1ee2b0a5e390982cb27b9b3ff7f95768be8164
Suleman Muqeed <sulemanmuqeed@gmail.com>
2022-09-18 19:02:11 +0500

    add required fields validation and fix cors

--- src/seashells/entities/seashell.entity.ts
+++ src/seashells/entities/seashell.entity.ts
@@ -1,16 +1,20 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class Seashell {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty()
   @Column()
   name: string;
 
+  @IsNotEmpty()
   @Column()
   species: string;
 
+  @IsNotEmpty()
   @Column()
   description: string;
 }
